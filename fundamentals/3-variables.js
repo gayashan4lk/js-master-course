006b3dc161783332f4c1f2db8f445cee15490e8e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-17 14:46:27 +0530

    lecture 3

---
+++ fundamentals/3-variables.js
@@ -0,0 +1,27 @@
+// JavaScript Variables
+
+var firstName = "Leonado";
+let secondName = "Davinci";
+const age = "52";
+console.log (firstName + secondName + age);
+
+// variable van start with letter, underscore or $
+// variables are case-SENSITIVE
+
+const leo = "Leonado";
+const LEO = "Lion";
+console.log(leo);
+console.log(LEO);
+
+//function
+/* prompt();
+let message = prompt("Enter something here :");
+alert (message); */
+
+// Undefined type
+var number;
+console.log(number); // undefined
+number = 5;
+console.log(number); // 5
+
+console.log(someVariableWhichIsNotExist); // throws an error : Uncaught Reference Error
