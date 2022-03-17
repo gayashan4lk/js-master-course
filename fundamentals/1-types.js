29f8138fdb9c46be39b3816af21070f5e4e16a43
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-17 12:47:07 +0530

    lec 1

---
+++ fundamentals/1-types.js
@@ -0,0 +1,14 @@
+// 1) Numbers
+console.log(3+7);
+console.log(10-9);
+console.log(10*3);
+console.log(20/5);
+console.log((6-2)*(4-2));
+console.log(10%3);
+
+// 2) Strings
+console.log('Leonado');
+console.log("Davinci");
+console.log('Leonado' + ' ' + 'Davinci');
+console.log("Leonando's Drawings");
+console.log('Leonandos Drawings');
