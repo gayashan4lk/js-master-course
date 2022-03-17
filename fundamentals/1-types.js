b1772a6cb7d22d5f799707ffbb12087690c279a7
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-17 13:50:26 +0530

    lecture 1

--- fundamentals/1-types.js
+++ fundamentals/1-types.js
@@ -1,14 +1,30 @@
 // 1) Numbers
-console.log(3+7);
-console.log(10-9);
-console.log(10*3);
-console.log(20/5);
-console.log((6-2)*(4-2));
-console.log(10%3);
+console.log(3 + 7);
+console.log(10 - 9);
+console.log(10 * 3);
+console.log(20 / 5);
+console.log((6 - 2) * (4 - 2));
+console.log(10 % 3);
 
 // 2) Strings
 console.log('Leonado');
-console.log("Davinci");
-console.log('Leonado' + ' ' + 'Davinci');
+console.log('Davinci');
+console.log('Leonado' + ' ' + 'Davinci'); //concatenation
 console.log("Leonando's Drawings");
-console.log('Leonandos Drawings');
+console.log('Leonando\'s Drawings'); //escape characters
+console.log(30 + ' years'); //33 is converted into string
+console.log(30 - '10'); //'10' is converted into number
+
+// 3) Boolean
+console.log(true);
+console.log(false);
+
+// 4) Null
+
+// 5) Undefined
+
+// 6) symbol
+
+// 7) Object
+
+// 8) Function
