d988565f156f4da051219d96a16c8d83e131e9a1
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-17 14:24:36 +0530

    lecture 2

---
+++ fundamentals/2-operators.js
@@ -0,0 +1,83 @@
+/*
+Arithmetic Operators
+**********************
++ Addition
+- Subraction
+* Multiplication
+** Exponentiation
+/ Division
+% Modulus (Division Remainder)
+++ increment
+-- Decrement
+
+Assignment Operators
+*************************
+=     (x = y) => (x =y)
++=    (x += y) => (x = x + y)
+-=    (x -= y) => (x = x - y)
+*=    (x *= y) => (x = x * y)
+/=    (x /= y) => (x = x / y)
+%=    (x %= y) => (x = x % y)
+**=   (x **= y) => (x = x ** y)
+
+Comparison Operators
+*********************
+==    equal to
+===   equal value and equal type
+!=    not equal
+!==   not equal value or not equal type
+>     greater than
+<     less than
+>=    greater than or equal to
+<=    less than or equal to
+?     ternary operator
+
+Logical operator
+*******************
+&&    logical AND
+||    logical OR
+!     logical NOT
+
+Type Operators
+***************
+typeof      : Returns the type of a variable
+instanceof  : Returns ture if an object is an instance of an object type
+
+Bitwise Operators
+********************
+&   AND
+|   OR
+~   NOT
+^   XOR
+<<  left shift
+>>  right shift
+>>> unsigned right shift
+
+String Operators (Concatenation)
+*********************************
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + lastName;
+
+let name = "Leonado";
+let name += "Davinci"
+
+ */
+console.log(5 > 5);
+console.log(5 == 5);
+console.log(5 < 5);
+console.log("***********");
+console.log(5 == 5 && 4 == 4);
+console.log(5 == 5 && 5 > 5);
+console.log(5 == 5 || 5 > 5);
+console.log(5 >= 5);
+console.log(5 != 5);
+console.log(!(5 == 5));
+console.log("*************");
+console.log(5 == '5');
+console.log(5 === '5');
+console.log('5' === '5'); // equal value and equal type
+console.log('5' !== 5); // not equal value or not equal type
+console.log(5 !== 6);
+console.log(6 !== "six");
+console.log(5 !== 5);
